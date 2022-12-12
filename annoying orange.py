d0f7541a69619c15defb92b85fb321f7dd7499b7
MNdanse <106297973+MNdanse@users.noreply.github.com>
2022-12-13 03:31:17 +0900

    Update annoying orange.py

--- annoying orange.py
+++ annoying orange.py
@@ -3,6 +3,8 @@ import dlib
 from imutils import face_utils, resize
 import numpy as np
 
+orange_img = cv2.imread('orange.png')
+orange_img = cv2.resize(orange_img, dsize=(512, 512))
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
@@ -24,8 +26,35 @@ while True :
         for p in shape:
             cv2.circle(face_img, center=(p[0] - x1, p[1] - y1), radius=2, color=255, thickness=-1)
 
+        #eyes
+        result = cv2.seamlessClone(
+            left_eye_img,
+            result,
+            np.full(left_eye_img.shape[:2], 255, left_eye_img.dtype),
+            (100, 200),
+            cv2.MIXED_CLONE
+        )
+
+        result = cv2.seamlessClone(
+            right_eye_img,
+            result,
+            np.full(right_eye_img.shape[:2], 255, right_eye_img.dtype),
+            (250, 200),
+            cv2.MIXED_CLONE
+        )
+        
+        #mouth
+        result = cv2.seamlessClone(
+            mouth_img,
+            result,
+            np.full(mouth_img.shape[:2], 255, mouth_img.dtype),
+            (180, 320),
+            cv2.MIXED_CLONE
+        )
+        
         
         cv2.imshow('face', face_img)
+        cv2.imshow('result', result)
 
     if cv2.waitKey(1) == ord('q'):
         break
