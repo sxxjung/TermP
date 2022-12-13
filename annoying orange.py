1bf343aebe2650239c1835a4f85ec530098d75dd
sxxjung <112802701+sxxjung@users.noreply.github.com>
2022-12-13 19:07:58 +0900

    Modify orange annoying.py

--- annoying orange.py
+++ annoying orange.py
@@ -13,6 +13,9 @@ while True :
     img = cv2.imread("face.jpg", cv2.IMREAD_COLOR)
 
     faces = detector(img)
+    
+    result = orange_img.copy()
+    
 
     if len(faces) > 0:
         face = faces[0]
@@ -25,6 +28,26 @@ while True :
         
         for p in shape:
             cv2.circle(face_img, center=(p[0] - x1, p[1] - y1), radius=2, color=255, thickness=-1)
+
+        
+         # eyes
+        le_x1 = shape[36, 0]
+        le_y1 = shape[37, 1]
+        le_x2 = shape[39, 0]
+        le_y2 = shape[41, 1]
+        le_margin = int((le_x2 - le_x1) * 0.18)
+
+        re_x1 = shape[42, 0]
+        re_y1 = shape[43, 1]
+        re_x2 = shape[45, 0]
+        re_y2 = shape[47, 1]
+        re_margin = int((re_x2 - re_x1) * 0.18)
+
+        left_eye_img = img[le_y1 - le_margin:le_y2 + le_margin, le_x1 - le_margin:le_x2 + le_margin].copy()
+        right_eye_img = img[re_y1 - re_margin:re_y2 + re_margin, re_x1 - re_margin:re_x2 + re_margin].copy()
+
+        left_eye_img = resize(left_eye_img, width=100)
+        right_eye_img = resize(right_eye_img, width=100)
 
         result = cv2.seamlessClone(
             left_eye_img,
@@ -42,6 +65,19 @@ while True :
             cv2.MIXED_CLONE
         )
         
+        # mouth
+        mouth_x1 = shape[48, 0]
+        mouth_y1 = shape[50, 1]
+        mouth_x2 = shape[54, 0]
+        mouth_y2 = shape[57, 1]
+        mouth_margin = int((mouth_x2 - mouth_x1) * 0.1)
+
+        mouth_img = img[mouth_y1 - mouth_margin:mouth_y2 + mouth_margin,
+                    mouth_x1 - mouth_margin:mouth_x2 + mouth_margin].copy()
+
+        mouth_img = resize(mouth_img, width=250)
+
+        
         result = cv2.seamlessClone(
             mouth_img,
             result,
@@ -50,7 +86,9 @@ while True :
             cv2.MIXED_CLONE
         )
         
-        
+        cv2.imshow('left', left_eye_img)
+        cv2.imshow('right', right_eye_img)
+        cv2.imshow('mouth', mouth_img)
         cv2.imshow('face', face_img)
         cv2.imshow('result', result)
 
